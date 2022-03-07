eb4401d0c940ff855054d09ea03fb41ad3279e34
Vitor Casteluci <vitor.casteluci@gmail.com>
2022-03-07 17:18:58 -0300

    Favorites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :favorite_text
 
   def favorite_text
     if @favorite_exists
-      'Unfavorite'
+      '/assets/heart_on.png'
     else
-      'Favorite'
+      '/assets/heart_off.png'
     end
   end
-  helper_method :favorite_text
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 
+# Favorites Controller
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:update]
   def update
-    favorite = Favorite.where(comic_id: params[:comic][:id])
-    if favorite == []
-      # Create favorite
+    favorite = Favorite.find_by(comic_id: params[:comic][:id])
+    if favorite.nil?
       Favorite.create(comic_id: params[:comic][:id], user: current_user)
       @favorite_exists = true
     else
-      # Delete the favorite
-      favorite.destroy_all
+      favorite.destroy
       @favorite_exists = false
     end
     respond_to do |format|
       format.html {}
       format.js {}
     end
+  end
+
+  private
+
+  def set_favorite
+    Favorite.find_by(comic_id: params[:comic][:id], user: current_user).nil?
   end
 end
