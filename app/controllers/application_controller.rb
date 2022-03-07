3fa84351954d3eda3ab1e9a50f7ef090a86d2f32
Vitor Casteluci <vitor.casteluci@gmail.com>
2022-03-07 17:54:59 -0300

    Favorite working on load now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :favorite_text
+  helper_method :favorite_img
+  helper_method :set_favorite
 
-  def favorite_text
+  def set_favorite(comic)
+    if Favorite.find_by(user_id: current_user.id, comic_id: comic[:id]).nil?
+      '/assets/heart_off.png'
+    else
+      '/assets/heart_on.png'
+    end
+  end
+
+  def favorite_img
     if @favorite_exists
       '/assets/heart_on.png'
     else
